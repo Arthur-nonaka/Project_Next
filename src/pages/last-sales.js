98df48c0c2fdec3ab6836d8b50824e2e00694645
teemago <65630024+teemago@users.noreply.github.com>
2024-03-03 11:43:03 -0300

    Pre-render class

--- src/pages/last-sales.js
+++ src/pages/last-sales.js
@@ -1,32 +1,67 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalePage() {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const [sales, setSales] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
+  const fetcher = (url) =>
+    fetch(url)
+      .then((res) => res.json());
+  const {data, isLoading, error} = useSWR(
+    "https://nextjs-course-2caa2-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+  
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-course-2caa2-default-rtdb.firebaseio.com/sales.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedData = [];
-        for (const key in data) {
-            transformedData.push({id: key, username: data[key].username, volume: data[key].volume});
-        }
-        setData(transformedData);
-        setIsLoading(false);
-      });
-  }, []);
+    if (data) {
+      const transformedData = [];
+      for (const key in data) {
+        transformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedData);
+    }
+  }, [data]);
 
-  if(isLoading) {
-    return <p>Loading...</p>
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://nextjs-course-2caa2-default-rtdb.firebaseio.com/sales.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const transformedData = [];
+  //       for (const key in data) {
+  //           transformedData.push({id: key, username: data[key].username, volume: data[key].volume});
+  //       }
+  //       setSales(transformedData);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Error</p>;
   }
 
-  if(!data) {
-    return <p>No Data yet</p>
+  if (!data) {
+    return <p>Loading...</p>;
   }
 
-  return <ul>{data.map(x => <li key={x.id}>{x.username} : {x.volume}</li>)}</ul>;
+  if (!sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((x) => (
+        <li key={x.id}>
+          {x.username} : {x.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default LastSalePage;
